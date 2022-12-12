4ec4b773a62f6932ab13fb0ecf24f151a102ab1f
Vlad <vladot@ya.ru>
2022-12-12 12:31:26 +0600

    Task1

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,52 @@
 public class Main {
     public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+        task4();
+
     }
+
+    public static void task1() {
+
+        System.out.println("Задача_1");
+        System.out.println();
+
+        int one[] = new int[]{1, 2, 3};
+        double two[] = {1.57d, 7.654d, 9.986d};
+        double three [] = {17, 28, 39};
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task2() {
+
+        System.out.println("Задача_2");
+        System.out.println();
+
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task3() {
+
+        System.out.println("Задача_3");
+        System.out.println();
+
+
+        System.out.println();
+        System.out.println();
+    }
+
+    public static void task4() {
+
+        System.out.println("Задача_4");
+        System.out.println();
+
+
+    }
+
 }
